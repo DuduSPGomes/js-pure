358f4f22e51186d7d63876d15558d59b82b24aad
Cadu <dudu.spgomes@gmail.com>
2022-02-26 17:15:47 -0300

    refactor: remove coments

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ import CounterProvider from "./context/index.js";
 import App from "./components/app.js";
 
 R.renderDOM(CounterProvider({ children: [Counter] }), document.body);
-
-// // transformar as funções que retornam o R.Component em um compoenent para re-renderizar seu escopo
-// // para os hooks dentro das funções funcionarem as funções precisam ser executadas novamente
-// // não é só renderizar os valores modificados no html, é precisa re-renderizar as funções que retornam os componentes
-
-// criar render parecido com useEffect, porém comparando o dom atual com o dom que será salvo ao chamar o renderDOM
